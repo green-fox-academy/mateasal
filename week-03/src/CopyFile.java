aac9d5aa1351cee640f5e2ea12d52d74899d3f02
Mate Sal <mate.andras.sal@gmail.com>
2019-04-16 14:58:56 +0200

    minor correction in copyfile

--- week-03/src/CopyFile.java
+++ week-03/src/CopyFile.java
@@ -11,7 +11,7 @@ public class CopyFile {
         // It should take the filenames as parameters
         // It should return a boolean that shows if the copy was successful
 
-        System.out.println(copyContents("assets/old-file.txt", "my-file.txt"));c
+        System.out.println(copyContents("assets/old-file.txt", "my-file.txt"));
     }
 
     public static boolean copyContents (String fileName1, String fileName2){
