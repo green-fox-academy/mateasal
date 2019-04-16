00594e5ded89c00c339b7c3f0ab43713a08f497e
Mate Sal <mate.andras.sal@gmail.com>
2019-04-16 14:56:45 +0200

    copyfile problem solved

--- week-03/src/CopyFile.java
+++ week-03/src/CopyFile.java
@@ -1,2 +1,33 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class CopyFile {
+
+    public static void main(String[] args) {
+
+        // Write a function that copies the contents of a file into another
+        // It should take the filenames as parameters
+        // It should return a boolean that shows if the copy was successful
+
+        System.out.println(copyContents("assets/old-file.txt", "my-file.txt"));
+    }
+
+    public static boolean copyContents (String fileName1, String fileName2){
+
+        try{
+            Path filePath1 = Paths.get(fileName1);
+            List<String> content1 = Files.readAllLines(filePath1);
+
+            Path filePath2 = Paths.get(fileName2);
+
+            Files.write(filePath2, content1);
+
+            return true;
+
+        } catch (Exception e){
+            return false;
+        }
+    }
 }
