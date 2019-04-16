269bdbe57ff7cd0323f5978bdc951ecc3cdd2050
Mate Sal <mate.andras.sal@gmail.com>
2019-04-16 22:26:06 +0200

    w2/d3 homework added

---
+++ week-03/day-03/src/Homework.java
@@ -0,0 +1,12 @@
+public class Homework {
+    public static void main(String[] args) {
+
+
+        String[][] Birch = {{"type"}, {"leaf color"}, {"age"}, {"sex"}};
+        String[][] Almond = {{"type"}, {"leaf color"}, {"age"}, {"sex"}};
+        String[][] Maple = {{"type"}, {"leaf color"}, {"age"}, {"sex"}};
+        String[][] Willow = {{"type"}, {"leaf color"}, {"age"}, {"sex"}};
+        String[][] Oak = {{"type"}, {"leaf color"}, {"age"}, {"sex"}};
+
+    }
+}
