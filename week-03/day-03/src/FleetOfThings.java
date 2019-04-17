c554db9cd682ddc862f00074713672974fea810c
Mate Sal <mate.andras.sal@gmail.com>
2019-04-17 14:42:11 +0200

    feat(FleetOfThings): task solved

---
+++ week-03/day-03/src/FleetOfThings.java
@@ -0,0 +1,29 @@
+public class FleetOfThings {
+
+    public static void main(String[] args) {
+        Fleet fleet = new Fleet();
+        // Create a fleet of things to have this output:
+        // 1. [ ] Get milk
+        // 2. [ ] Remove the obstacles
+        // 3. [x] Stand up
+        // 4. [x] Eat lunch
+
+        Fleet newFleet = new Fleet();
+
+        Thing item1 = new Thing("Get milk");
+        Thing item2 = new Thing("Remove the obstacles");
+        Thing item3 = new Thing("Stand up");
+        Thing item4 = new Thing("Eat lunch");
+
+
+        newFleet.add(item1);
+        newFleet.add(item2);
+        newFleet.add(item3);
+        newFleet.add(item4);
+
+        item3.complete();
+        item4.complete();
+
+        System.out.println(newFleet);
+    }
+}
