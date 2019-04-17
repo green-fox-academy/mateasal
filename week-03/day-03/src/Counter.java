2aa6a616ded447dbec09b01595c3566f859e9c50
Mate Sal <mate.andras.sal@gmail.com>
2019-04-17 12:43:51 +0200

    feat(Counter): task solved

---
+++ week-03/day-03/src/Counter.java
@@ -0,0 +1,26 @@
+public class Counter {
+
+    int count;
+    int initialValue;
+
+    public Counter(int count){
+        this.count = count;
+        initialValue = count;
+    }
+
+    public void add (int number){
+        count += number;
+    }
+
+    public void add(){
+        count++;
+    }
+
+    public int get(){
+        return count;
+    }
+
+    public void reset(){
+        count = initialValue;
+    }
+}
