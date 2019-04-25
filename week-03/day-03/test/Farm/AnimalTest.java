81dd359350de64bacc71025318f658eb98c951a2
Mate Sal <mate.andras.sal@gmail.com>
2019-04-25 17:29:09 +0200

    test(animal, sharpies) tasks tested

---
+++ week-03/day-03/test/Farm/AnimalTest.java
@@ -0,0 +1,20 @@
+package Farm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AnimalTest {
+
+    @Test
+    public void eat() {
+    }
+
+    @Test
+    public void drink() {
+    }
+
+    @Test
+    public void play() {
+    }
+}
